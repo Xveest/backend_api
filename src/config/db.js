c1125e57894af40ee8bbd4007e2e3915d66f07c5
Xveest <diegoespisalinas@gmail.com>
2026-04-01 07:17:16 -0600

    cadena de conexion para entorno de produccion

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,24 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-   
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
 
+const pool = new Pool(
+    process.env.DATABASE_URL 
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+    : {
+        user: process.env.DB_USER,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST
+    }
+);
 
 pool.on('connect', () => console.log('Conexion a la BD exitosa'));
 
 module.exports = pool;
-
